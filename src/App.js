17adf49213a45c24aba71b904e9c5863df0ab7da
gahlot1999 <gahlot1999@gmail.com>
2024-01-17 15:06:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [userInfo, setUserInfo] = useState([]);
+  useEffect(() => {
+    async function getInfo() {
+      try {
+        const res = await fetch('/api/info');
+        if (!res.ok) {
+          throw new Error('Failed fetching user information.');
+        }
+        const data = await res.json();
+        setUserInfo(data.users);
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getInfo();
+  }, []);
+
+  console.log(userInfo);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}
+    >
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        userInfo.map((user) => (
+          <div style={{ backgroundColor: 'lightpink' }} key={user.id}>
+            <p>Name: {user.name}</p>
+            <p>Email: {user.email}</p>
+            <p>Age: {user.age}</p>
+            <p>
+              Address:{' '}
+              {`${user.address.city}, ${user.address.state}, ${user.address.country}`}
+            </p>
+          </div>
+        ))
+      )}
     </div>
   );
 }
