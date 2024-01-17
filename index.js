e563f7dc4a6a40e07bbaf36e7a585d2f57ef8f9b
gahlot1999 <gahlot1999@gmail.com>
2024-01-17 17:07:48 +0530

    updated cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
